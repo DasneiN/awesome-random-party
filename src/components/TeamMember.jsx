c0c287549f6685fb3c6ceb56dc80a50d06d2f2c3
Dzmitry Tsebruk <mikhama@tut.by>
2019-01-25 21:56:02 +0300

    fix: fix typo in property name

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -5,7 +5,7 @@ class TeamMember extends Component {
     const { github, name, avatar } = this.props;
 
     return (
-      <div clasName="team-member">
+      <div className="team-member">
         <img src={avatar} alt={`${name}'s avatar`} width="100" height="100" />
         <span>{name}</span>
         <a href={`https://github.com/${github}`}>{github}</a>
