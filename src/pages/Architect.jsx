e4e56f2324c87d8bea5464d6d539a120bbe7d0f7
Dzmitry Tsebruk <mikhama@tut.by>
2019-01-27 11:16:17 +0300

    feat: add component Gallery

--- src/pages/Architect.jsx
+++ src/pages/Architect.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ArchitectInfo from '../components/ArchitectInfo';
 import TimelineBio from '../components/TimelineBio';
 import Video from '../components/Video';
+import Gallery from '../components/Gallery';
 
 class Architect extends Component {
   render() {
@@ -12,6 +13,7 @@ class Architect extends Component {
         <ArchitectInfo person={person} />
         <TimelineBio person={person} />
         <Video person={person} />
+        <Gallery person={person} />
       </>
     );
   }
