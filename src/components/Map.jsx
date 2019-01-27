4cb024d55643cea6b0e1ae7fc9bde5fef6fdcb00
HaspadarZ-padMensku <gonchds@gmail.com>
2019-01-28 01:29:01 +0300

    feat: add markup and styles to Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import uniqid from 'uniqid';
-import { Trans } from 'react-i18next';
+
+import '../styles/map.css';
 
 class Map extends Component {
   render() {
     const { person } = this.props;
 
     return (
-      <>
-        <h3><Trans>BirthPlace</Trans></h3>
+      <section id="map" className="person-page map-box">
         <iframe title={uniqid()} className="maps" src={person.mapSrc.url} frameBorder="0" style={{ border: 0 }} allowFullScreen />
-      </>
+      </section>
     );
   }
 }
