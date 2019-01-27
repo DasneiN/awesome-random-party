7a73783588ee5161d7b82899a6d92d9accb13118
HaspadarZ-padMensku <gonchds@gmail.com>
2019-01-27 23:26:38 +0300

    feat: add markup and styles to TimelineBio component

--- src/components/TimelineBio.jsx
+++ src/components/TimelineBio.jsx
@@ -3,30 +3,35 @@ import React, { Component } from 'react';
 import uniqid from 'uniqid';
 import { Trans } from 'react-i18next';
 
+import '../styles/timeline.css';
+
 class TimelineBio extends Component {
   render() {
     const { person } = this.props;
 
     return (
-      <div className="timeline">
-        <h2><Trans>TimelineBio</Trans></h2>
-        <Timeline lineColor="#ddd">
-          {
-            person.biography.map(timePoint => (
-              <TimelineItem
-                key={uniqid()}
-                dateText={timePoint.period}
-                style={{ color: '#e86971' }}
-              >
-                <p>
-                  {timePoint.information}
-                </p>
-              </TimelineItem>
-            ))
-          }
-        </Timeline>
-      </div>
+      <section className="person-page timeline">
+        <h3 className="timeline-title"><Trans>TimelineBio</Trans></h3>
+        <div id="timeline" className="timeline-container">
+          <Timeline lineColor="#ddd">
+            {
+              person.biography.map(timePoint => (
+                <TimelineItem
+                  key={uniqid()}
+                  dateText={timePoint.period}
+                  style={{ color: '#e86971' }}
+                >
+                  <p>
+                    {timePoint.information}
+                  </p>
+                </TimelineItem>
+              ))
+            }
+          </Timeline>
+        </div>
+      </section>
     );
   }
 }
+
 export default TimelineBio;
